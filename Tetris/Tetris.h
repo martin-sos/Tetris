1e832506198e366acb724c6e661ef679469ef514
martin-sos <98313614+martin-sos@users.noreply.github.com>
2022-10-31 15:21:05 +0100

    all tabs to spaces

--- Tetris/Tetris.h
+++ Tetris/Tetris.h
@@ -17,9 +17,9 @@ struct Field {
 };
 
 class Tetris {
-private:		
+private:        
 
-    std::vector<std::vector<Field>> game_field;		// the lower left corner of the game field is defined as the entry at row 0, column 0
+    std::vector<std::vector<Field>> game_field;     // the lower left corner of the game field is defined as the entry at row 0, column 0
     
     bool isActive;                          // is a game active?
     bool isPaused;                          // is a game paused?
@@ -28,22 +28,22 @@ private:
     Tetris_Stats_entry entry;
     Tetris_Statistics* stats;
     
-    Tetromino nextTetromino;				// the TetrominoKind which is added next to the game field
+    Tetromino nextTetromino;                // the TetrominoKind which is added next to the game field
     Tetromino currentTetromino;
 
     
-    void run();								// executes the Tetris game loop; is exectued in a thread
-    void detectKeyboardInput();				// listens for keyboard input; is executed in a thread
+    void run();                             // executes the Tetris game loop; is exectued in a thread
+    void detectKeyboardInput();             // listens for keyboard input; is executed in a thread
     
-    bool fall();									// lets minos fall down one more line, if there is nothing that could fall, return false, otherwise true
-    bool shift(const MoveTetromino);				// ...
+    bool fall();                                    // lets minos fall down one more line, if there is nothing that could fall, return false, otherwise true
+    bool shift(const MoveTetromino);                // ...
     bool tryShift(const MoveTetromino, Tetromino);  // ...
-    void rotate(const RotateTetromino);				// rotate a TetrominoKind Left or Right
+    void rotate(const RotateTetromino);             // rotate a TetrominoKind Left or Right
     
-    void destroyLine(void);					// detects a full line, destroys it and will let everything above fall down one more line
-    void placeNextTetromino(void);			// sets nextTetromino at the top of the game field and creates a new Tetromino
+    void destroyLine(void);                 // detects a full line, destroys it and will let everything above fall down one more line
+    void placeNextTetromino(void);          // sets nextTetromino at the top of the game field and creates a new Tetromino
     void placeCurrentTetromino(void);
-    void draw(void);						// draws the field
+    void draw(void);                        // draws the field
     void print_stats(void);
     void clearBoard(void);
     
@@ -68,7 +68,7 @@ public:
         delete stats;
     }
 
-    void start();				// starts a Tetris game, spawn all the needed threads, terminates as soon as game is over
+    void start();               // starts a Tetris game, spawn all the needed threads, terminates as soon as game is over
 
 };
 
