c582694f15bbc426e2c5b210ab306d9a39756b45
Martin Sosniak <martin.sosniak@mailbox.org>
2022-11-24 14:21:25 +0100

    moving construction of Tetris_Draw object into factory to release main from os-specific stuff (at least partially)

---
+++ Tetris/Tetris_Draw_Console_Factory.h
@@ -0,0 +1,25 @@
+#ifndef TETRIS_DRAW_CONSOLE_FACTORY_H
+#define TETRIS_DRAW_CONSOLE_FACTORY_H
+#include <memory>
+#ifdef __unix__
+#include "Tetris_Draw_Linux_Console.h"
+#elif (defined (_WIN32) || defined (_WIN64))
+#include "Tetris_Draw_Windows_Console.h"
+#endif
+
+
+
+class Tetris_Draw_Console_Factory
+{
+public:
+    static std::unique_ptr<Tetris_Draw> create()
+    {
+    #ifdef __unix__
+        return std::unique_ptr<Tetris_Draw>(new Tetris_Draw_Linux_Console());
+    #elif (defined (_WIN32) || defined (_WIN64))
+        return std::unique_ptr<Tetris_Draw>(return new etris_Draw_Windows_Console());
+    #endif
+    }
+};
+
+#endif // TETRIS_DRAW_CONSOLE_FACTORY_H
